456c59d3a96494a09b371ea6394c7e2971729e98
Curtis Wilkinson <curtis@codafication.com>
2017-09-08 08:46:09 +1000

    Testing kafka connection and topic consume

--- orders/src/main.rs
+++ orders/src/main.rs
@@ -1,3 +1,40 @@
+extern crate kafka;
+extern crate env_logger;
+
+use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
+use kafka::error::Error as KafkaError;
+
 fn main() {
-    println!("/Orders rust is running!")
+    env_logger::init().unwrap();
+    let broker = "localhost:9092".to_owned();
+    let topic = "test".to_owned();
+    let group = "group".to_owned();
+
+    if let Err(e) = consume(group, topic, vec![broker]) {
+        println!("Failed consuming messages: {}", e);
+    }
+}
+
+fn consume(group: String, topic: String, brokers: Vec<String>) -> Result<(), KafkaError> {
+    let mut connection = try!(Consumer::from_hosts(brokers)
+                              .with_topic(topic)
+                              .with_group(group)
+                              .with_fallback_offset(FetchOffset::Earliest)
+                              .with_offset_storage(GroupOffsetStorage::Kafka)
+                              .create());
+    loop {
+        let mss = try!(connection.poll());
+
+        if mss.is_empty() {
+            println!("No Messages!");
+        }
+
+        for ms in mss.iter() {
+            for m in ms.messages() {
+                println!("{}:{}@{}: {:?}", ms.topic(), ms.partition(), m.offset, m.value);
+            }
+            let _ = connection.consume_messageset(ms);
+        }
+        try!(connection.commit_consumed());
+    }
 }
